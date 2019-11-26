7c39321f1f5726008536076cc02d52bd624def78
isWarrens <1489070715@qq.com>
2019-11-25 17:20:33 -0800

    updata

---
+++ week11/code/my.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<signal.h>
+#include <wait.h>
+#include <time.h>
+#include<sys/stat.h>
+#include<signal.h>
+#include<sys/param.h>
+void init_daemon(void);
